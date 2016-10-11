65ea8a2b89fdce1ed5f8e0565169952305ad3867
kidok <kidokchen@gmail.com>
2016-10-11 22:55:13 +0800

    update

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 //mongodb connection
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/testusers');
+mongoose.connect('mongodb://mongodb/testusers');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
